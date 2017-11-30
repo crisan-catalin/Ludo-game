1195a054886a88719a940a08a4bcd8b3d1011750
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2017-11-30 18:14:01 +0200

    Casuta: add form property for cell position

--- WindowsFormsControlLibrary1/Casuta.cs
+++ WindowsFormsControlLibrary1/Casuta.cs
@@ -14,6 +14,10 @@ namespace Casuta.UserControl
     {
         public TipCasuta TipCasuta { get; set; }
         public bool EsteOcupata { get; set; }
+        public int PozitieRosu { get; set; }
+        public int PozitieVerde { get; set; }
+        public int PozitieAlbastru { get; set; }
+        public int PozitieGalben { get; set; }
 
         public Casuta()
         {
