fc2e413ce782c2d3c869f340b2f2fa195189035a
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2017-12-01 21:15:51 +0200

    Add Zar Model

---
+++ Proiect_AppUI/Model/Zar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proiect_AppUI.Model
+{
+    public class Zar
+    {
+        public int AruncaZar
+        {
+            get { return new Random().Next(1, 7); }
+        }
+    }
+}
