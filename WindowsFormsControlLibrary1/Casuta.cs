e59c0eac8585ab404f9c1cc1c6ced6abed434e92
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2017-12-02 17:59:42 +0200

    Casuta: Add method for set position and make property private set

--- WindowsFormsControlLibrary1/Casuta.cs
+++ WindowsFormsControlLibrary1/Casuta.cs
@@ -5,16 +5,24 @@ namespace Casuta.UserControl
     public partial class Casuta : System.Windows.Forms.UserControl
     {
         public bool EsteOcupata { get; set; }
-        public int PozitieRosu { get; set; }
-        public int PozitieVerde { get; set; }
-        public int PozitieAlbastru { get; set; }
-        public int PozitieGalben { get; set; }
+        public int PozitieRosu { get; private set; }
+        public int PozitieVerde { get; private set; }
+        public int PozitieAlbastru { get; private set; }
+        public int PozitieGalben { get; private set; }
         public event EventHandler PionCliked;
         public event EventHandler PionMouseEnter;
 
         public Casuta()
         {
             InitializeComponent();
+        }
+
+        public void SeteazaPozitieCasuta(int pozitieRosu, int pozitieVerde, int pozitieGalben, int pozitieAlbastru)
+        {
+            PozitieRosu = pozitieRosu;
+            PozitieVerde = pozitieVerde;
+            PozitieAlbastru = pozitieAlbastru;
+            PozitieGalben = pozitieGalben;
         }
 
         protected virtual void OnPionClicked(EventArgs e)
