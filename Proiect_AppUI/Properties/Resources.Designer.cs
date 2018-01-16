3579bb84b6784b31f190a6c0513ee2dfb32a6c7d
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-01-16 20:36:08 +0200

    Update project files

--- Proiect_AppUI/Properties/Resources.Designer.cs
+++ Proiect_AppUI/Properties/Resources.Designer.cs
@@ -324,16 +324,6 @@ namespace Proiect_AppUI.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap logo {
-            get {
-                object obj = ResourceManager.GetObject("logo", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
         /// </summary>
         internal static System.IO.UnmanagedMemoryStream move {
